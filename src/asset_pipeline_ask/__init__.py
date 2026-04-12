d931e67f16bff729a888b07295877f60148f469c
Andrew S Klug // ASK <andrew@ASKproduKtion.com>
2026-04-12 14:30:38 -0700

    Add deterministic output validation seam to main line

--- src/asset_pipeline_ask/__init__.py
+++ src/asset_pipeline_ask/__init__.py
@@ -1,3 +1,9 @@
 from .ingest_validation import IngestValidationResult, validate_ingest
+from .output_validation import OutputValidationResult, validate_outputs
 
-__all__ = ["IngestValidationResult", "validate_ingest"]
+__all__ = [
+    "IngestValidationResult",
+    "OutputValidationResult",
+    "validate_ingest",
+    "validate_outputs",
+]
